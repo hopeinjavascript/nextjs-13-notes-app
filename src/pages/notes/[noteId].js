6aaed306ffd58c7614d6b4ee6ca1be11430b7de4
hopeinjavascript <hopeinjavascript@gmail.com>
2023-06-12 12:57:28 +0530

    feat: get a single note

---
+++ src/pages/notes/[noteId].js
@@ -0,0 +1,60 @@
+import React from 'react';
+import { notes } from '@/../data/notes';
+import styles from '@/styles/SingleNote.module.css';
+
+const Note = ({ note }) => {
+  return (
+    <div className={styles.note} key={note.id}>
+      <h1 className={styles.text}>{note.text}</h1>
+      <p className={styles.desc}>{note.desc}</p>
+    </div>
+  );
+};
+
+export async function getStaticPaths() {
+  /**
+   * Server Error
+    Error: A required parameter (noteId) was not provided as a string received number in getStaticPaths for /notes/[noteId]
+    This error happened while generating the page. Any console logs will be displayed in the terminal window.
+  **/
+
+  // noteId refers to the file name
+  return {
+    paths: [
+      { params: { noteId: '1' } }, // values should be string
+      { params: { noteId: '2' } },
+      { params: { noteId: '3' } },
+      { params: { noteId: '4' } },
+    ],
+    fallback: false,
+  };
+}
+
+export async function getStaticProps(context) {
+  const {
+    params: { noteId },
+  } = context;
+
+  /*
+  DO THIS when you already have data available
+  */
+  const note = notes.find((note) => note.id === parseInt(noteId));
+
+  //destructing because it is giving error of serialization on date object
+  const { id, text, desc, status, createdBy } = note;
+
+  /*
+  DO NOT DO THIS
+  we should not call our own API routes for pre-rendering content as it might cause unnecessary roundtrip/delay, this can save you approx 100ms
+  The fact is that it is advised/recommended not to call our own API routes from getStaticProps or getServerSideProps
+  you may call any external API routes
+  */
+  // const res = await fetch(`/api/notes/${noteId}`);
+  // const note = await res.json();
+
+  return {
+    props: { note: { id, text, desc, status, createdBy } },
+  };
+}
+
+export default Note;
