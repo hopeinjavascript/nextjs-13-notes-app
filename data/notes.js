5ebdce866b6d8d21ed3c46282738819d5648b121
hopeinjavascript <hopeinjavascript@gmail.com>
2023-06-12 13:28:09 +0530

    chore: add a new note data object in data file

--- data/notes.js
+++ data/notes.js
@@ -26,4 +26,13 @@ export const notes = [
     createdAt: new Date(),
     updatedAt: new Date(),
   },
+  {
+    id: 4,
+    text: 'Note 4',
+    desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    status: 'completed', // new | progress | completed
+    createdBy: 'user 4',
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  },
 ];
