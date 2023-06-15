38bc8cc077995ac7c22d3acec39b27d09bba2102
hopeinjavascript <hopeinjavascript@gmail.com>
2023-06-15 19:54:04 +0530

    feat: add context for notes

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,10 @@
-import '@/styles/globals.css'
+import NotesContextProvider from '@/context/notes';
+import '@/styles/globals.css';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <NotesContextProvider>
+      <Component {...pageProps} />
+    </NotesContextProvider>
+  );
 }
